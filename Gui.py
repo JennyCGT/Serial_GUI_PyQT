20e395ef794e4fcdb84eb64b59e04bfda56704dd
JennyCGT <jenniferguayta@gmail.com>
2020-06-16 23:47:51 -0500

    V2.0

--- Gui.py
+++ Gui.py
@@ -35,17 +35,12 @@ stop_threads_1 = False
 flag_data=False
 flag_save= False
 line =1
-analog =0
-dato1=0
 event = Event()
 data_serial = [b'', b'', b'',b'']
 
 # Class for serial Comunication 
 class Serial_com:
     def __init__(self, port, baud):
-        self.running = 1
-        self.analog =0
-        self.dato1=0
         self.ser= serial.Serial(port,baud,\
         parity=serial.PARITY_NONE,\
         stopbits=serial.STOPBITS_ONE,\
@@ -57,6 +52,7 @@ class Serial_com:
         # Thread for reading serial Port
         self.t1 = Thread(target = self.loop)
         self.t1.start()
+        # Thread for synchronize the two data incoming
         self.t2 = Thread(target = self.update)
         self.t2.start()
 
@@ -67,9 +63,7 @@ class Serial_com:
         while True:
             global stop_threads, data_serial, event
             if stop_threads: 
-                break
-                # stop_threads_1= True
-            
+                break            
             # Waiting for serial buffer
             if self.ser.inWaiting() > 0:    
                 # a=ser.read_until(ETX,8)
@@ -87,38 +81,31 @@ class Serial_com:
                 data_serial =c
                 if(data_serial[0]==b'R'):
                     data.save(data_serial[2],0)
-                    # data.axis_data1 = data_serial[2]
-                    # print (data.axis_data1)
                 if(data_serial[0]==b'B'):
                     data.save(data_serial[2],1)                
                 look.release()
-                # flag_data = True
-                # event.set()
         self.ser.close()    
     
     def update (self):
-        i = 0
         while True:
             global  event
             if stop_threads:
                 break
+            # Wait for the two data are received
             if event.is_set():
-                # print(data.tim)
-                # print(data.axis_t[-1],data.axis_data1[-1], data.axis_data2[-1])
+                # Update the current data in GUI
                 frame.update()
                 global flag_save, line
+                # If rec button was pressed, the data are saved in CSV file
                 if(flag_save):
-                    # print('guardar')
                     data_save=[str(line),data.tim ,str(frame.baud_selec),str(data.data1),str(data.data2)]
-                    # print(data_save)
+                    # function for save data
                     append_list_as_row(frame.path_dir,frame.data_rec, data_save)
                     line = line+1
-
                 event.clear()
 
-# Lists serial port names
-# A list of the serial ports available on the system
 
+# A list of the serial ports available on the system
 def serial_ports():
     if sys.platform.startswith('win'):
         ports = ['COM%s' % (i + 1) for i in range(256)]
@@ -165,15 +152,12 @@ class Screen(QWidget):
         self.path_dir = 'C:'
         self.data_rec=''
         self.time_upd= 0.5
-        # panel = self
 
         grid = QGridLayout()
         grid.setColumnStretch(0, 2)
         grid.setColumnStretch(1, 1)        
         
-        # grid.setRowStretch(0,1)        
         grid.setRowStretch(2,5)
-        # grid.setRowStretch(2,1)
 
         grid.setRowMinimumHeight(0,1)
 
@@ -188,12 +172,10 @@ class Screen(QWidget):
 
         self.setFixedSize(1000,600)
         self.setWindowTitle('Datalogger')
-        # panel.SetBackgroundColour('#364958')
-        # panel.SetBackgroundColour('#5B5F97')
         self.show()
 
 
-# --------------------------------BOX SERIAL SETTINGS-----------------------------------------------------------
+# ----------------------BOX SERIAL SETTINGS--------------------------------------------------
     def serial_settings(self):
         self.box_serial = QGroupBox("Serial Settings")
         text_port = QLabel("Port")
@@ -254,7 +236,7 @@ class Screen(QWidget):
         # self.box_rec.setStyleSheet("background-color: #F1F7EE")
         return self.box_rec
 
-# ------------------ PLOT SETTINGS ------------------------------------------------------------
+# ----------------------PLOT SETTINGS ---------------------------------------------------------
     def plot_settings(self):
         self.box_plot = QGroupBox("Real Time Graph")
         self.fig = Figure(figsize=([6.2,5]),tight_layout = {'pad': 2})
@@ -307,30 +289,24 @@ class Screen(QWidget):
         text_data1= QLabel("Y-Limit Max")
 
         self.Limit_max = QSpinBox()
-        # self.value_data1.setMaximun
         self.Limit_max.setRange(-10000,10000)
         self.Limit_max.setSingleStep(10)
         self.Limit_max.setValue(100)
-        # self.Limit_max.setStyleSheet('background-color: white')
-        
         
         text_data2 = QLabel("Y-Limit Min")
         self.Limit_min = QSpinBox()
         self.Limit_min.setRange(-10000,10000)
         self.Limit_min.setSingleStep(10)
         self.Limit_min.setValue(0)
-        # self.Limit_min.setStyleSheet('background-color: white')
 
         self.time = QSpinBox()
         self.time.setRange(400,10000000)
         self.time.setSingleStep(100)
         self.time.setValue(400)
-        # self.time.setStyleSheet('background-color: white')
         text_data3 = QLabel("Time to update")
         text_data4 = QLabel("[ms]")
 
         self.set_button = QPushButton('SET') 
-        # self.set_button.setStyleSheet('background-color: #F2F2F2')
         self.set_button.clicked.connect(self.Set_Limit)
 
         b1 = QHBoxLayout()
@@ -358,10 +334,8 @@ class Screen(QWidget):
         b3.addStretch(1)
         b3.addWidget(self.set_button)
         b3.addStretch(1)
-        # self.box_data.setLayout(b1)
         self.box_limit = QGroupBox("Graph Settings")
         self.box_limit.setLayout(b3)
-        # self.box_limit.setStyleSheet("background-color: #87BBA2")    
         return self.box_limit
     
 # -------------------- MESSAGE SETTINGS -----------------------------------------------------
@@ -394,23 +368,20 @@ class Screen(QWidget):
         self.box_msg.setLayout (b1)
         return self.box_msg
 
-#------------------------------------------------------------------------------------------------
+#---------------------- FUNCTIONS -------------------------------------------------
     def showDialog(self):
         msgBox = QMessageBox()
         msgBox.setIcon(QMessageBox.Information)
-        msgBox.setText("Alert")
-        msgBox.setWindowTitle("Choose a valid port")
+        msgBox.setWindowTitle("Alert")
+        msgBox.setText("Choose a valid port")
         msgBox.setStandardButtons(QMessageBox.Ok )
 
         returnValue = msgBox.exec()
     
     def onRec(self,event):
         # ------------ Function for export data
-        # print('rec')
-        # print(self.rec_button.Label)
         if self.rec_button.text()=='REC':
             print('rec')
-            # self.path_dir= os.path.abspath(os.getc wd())
             self.data_rec = datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
             self.text_msg.setText('Exporting file ...'+ self.data_rec+'.csv')
             # f=self.path_dir+"\\"+self.data_rec+'.csv'
@@ -439,37 +410,34 @@ class Screen(QWidget):
         self.port.clear()
         ports = list(serial.tools.list_ports.comports())
         lst = []
-
         for p in ports:
-            # print (type(p.device))
             self.port.addItem(p.device)
 
     # Get de Baudrate selected
     def selec_baud(self,text):
         self.baud_selec = self.baud.currentText() 
-        print( type(self.baud_selec))
-        # print(text)
-
+    
     # Get Port Selected 
     def selec_port(self,text):
         self.port_selec = self.port.itemText(text)
-        print(type(self.port_selec))
     
     # Start thread of Serial Communication
     def onConnect(self, event):
         global stop_threads,stop_threads_1, flag_data 
         print('port: '+ self.port_selec +'Baud: '+self.baud_selec)
-        # print(self.connect_button.text())
+        # Detect if the port was selected
         if self.connect_button.text()=='Connect':
             if(self.port_selec == '' or self.port_selec == 'Choose a port'):
                 self.showDialog()
             else:
+                # Start Serial protocol
                 self.connect_button.setText('Disconnect')
                 stop_threads = False
                 stop_threads_1 = False
                 print('Start')
                 self.Serial=Serial_com(self.port_selec,self.baud_selec)
                 self.ser_msg.setText("Open")
+                # Disable the options for port and baudrate
                 self.port.setDisabled(True)
                 self.baud.setDisabled(True)
                 flag_data = True
@@ -478,7 +446,6 @@ class Screen(QWidget):
             stop_threads = True
             flag_data = False
             self.ser_msg.setText("Close")            
-            # self.Serial.endApplication()
             self.port.setDisabled(False)
             self.baud.setDisabled(False)
     
@@ -490,9 +457,8 @@ class Screen(QWidget):
         self._plot.y_max = self.y_max
         self._plot.y_min = self.y_min
         self._plot._time = self.time_upd
-        # print(self.y_max)
-        # print(self.y_min)
 
+    # Update the Current values on the GUI
     def update(self):
         self.value_data1.setText(str(data.data1))
         self.value_data2.setText(str(data.data2))
@@ -524,12 +490,9 @@ class DataPlot:
     def save_data(self, data1,data2):
         self.tim=datetime.now().strftime('%Y %m %d %H:%M:%S.%f')
         ######## DATA1 ##########################
-        # print(self.axis_t)
         self.data1= data1
-        # print(self.axis_data1)
         ######## DATA2 ##############
         self.data2= data2
-        # print(self.axis_data2)
 
     # Wait for get two data form serial before save
     def save (self,a,i):
@@ -551,92 +514,44 @@ class RealtimePlot:
         self.a = a
         self.canvas = canvas
         self.fig = fig
-        self.lineplot, = self.a.plot([],[],'ro-', label="Data1",markersize=1, linewidth=1)
-        self.lineplot1, = self.a.plot( [],[],'bo-', label="Data2",markersize=1,linewidth=1)
+        self.lineplot, = self.a.plot([],[],'ro-', label="Analogue1",markersize=1, linewidth=1)
+        self.lineplot1, = self.a.plot( [],[],'bo-', label="Analogue2",markersize=1,linewidth=1)
         
         self.a.set_ylim([self.y_min , self.y_max ])
         y=np.arange(self.y_min, self.y_max+5,10)
         self.a.set_yticks(y)
-
-        self.a.legend(loc=1) 
-        # self.a.minorticks_on()
-        # self.a.grid(which='major', linestyle='-', linewidth='0.5', color='black') 
-        # self.a.grid(which='minor', linestyle=':', linewidth='0.5', color='black') 
-
+        self.a.legend(loc=2,fontsize="x-small") 
         self.fig.canvas.draw()
-        # self.animator = manim.FuncAnimation(self.fig,self.anim, interval=500,)
-        # self.axbackground = self.fig.canvas.copy_from_bbox(self.a.bbox)
-        # plt.show(block=True)
-
         self.t3= Thread(target = self.loop)
         self.t3.start()
     
-    # Plotting Real timeF
+    # Plotting Real time
     def loop (self):
         while True:
             global stop_threads_1, event, flag_data
             if stop_threads_1:
                 break
-            # print(data.axis_data1)
-            # print(data.axis_data2)
+            # Update data for graph
             if flag_data:
                 data.save_all(data.data1,data.data2,data.tim)
-            # self.animator = manim.FuncAnimation(self.fig,self.anim, interval=500,blit=True)
             
             self.anim()
-            # print(self._time-0.3)
             time.sleep(self._time-0.3)
-                # time.sleep(float(self._time))
     
     def anim (self):
-        # start = time.process_time()
-
-        # if flag_data:
-        #     data.save_all(data.data1,data.data2,data.tim)
-        # Update Data to the plot
-
-        # y=np.arange(self.y_min, self.y_max+5,10)
-        # self.a.set_yticks(y)
-
-        # self.lineplot.set_data(np.arange(0,len(data.axis_data1),1),np.array(data.axis_data1))
-        # self.lineplot1.set_data(np.arange(0,len(data.axis_data2),1),np.array(data.axis_data2))
-
-        # self.a.clear()
-        # self.fig.canvas.restore_region(self.axbackground)
-        # self.fig.canvas.blit(self.a.bbox)
-        # self.a.draw(self.lineplot)
-        # self.a.draw(self.lineplot1)
-
-
-        # self.a.plot(list(range(len(data.axis_data1))),data.axis_data1,'ro-', label="Analogue 1",markersize=1, linewidth=1)
-        # self.a.plot(list(range(len(data.axis_data2))),data.axis_data2,'bo-', label="Analogue 2",markersize=1,linewidth=1)
-
+        # Refresh the data, labels and limits in the graph
         self.lineplot.set_data(np.arange(0,len(data.axis_data1),1),np.array(data.axis_data1))
         self.lineplot1.set_data(np.arange(0,len(data.axis_data2),1),np.array(data.axis_data2))
-
         self.a.draw_artist(self.lineplot)
         self.a.draw_artist(self.lineplot1)
-
         self.a.set_xticklabels(data.axis_t, fontsize=8)
-
         self.a.set_ylim([self.y_min , self.y_max ])
         y=np.arange(self.y_min, self.y_max+5,10)
         self.a.set_yticks(y)
         self.a.autoscale_view(True)   
         self.a.relim()
-
-
-
-        # self.a.set_xticklabels(data.axis_t, fontsize=8)
-        # plt.show(0.1)
-        # self.fig.canvas.blit() 
         self.fig.canvas.draw()
-        # self.fig.canvas.update()
-
         self.fig.canvas.flush_events()
-        
-        # stop = time.process_time()
-        # print(stop-start)
 
 
 
@@ -648,9 +563,7 @@ if __name__ == '__main__':
     # GUI panel
     app = QApplication([])
     frame = Screen()
-    # _plot = RealtimePlot(frame.a,frame.canvas, frame.fig)
     app.exec_()
-    # sys.exit(app.exec_())
     stop_threads_1 = True 
     stop_threads = True 
     print("exit")
